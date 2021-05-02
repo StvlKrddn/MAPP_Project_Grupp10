0d0376335270367ac2162d598d0d07e1359ef9a3
babyboidaniel <danielspider2002@gmail.com>
2021-05-02 14:17:07 +0200

    rock

--- MAPP Projekt Grupp10/Assets/Scripts/ThrowRockScript.cs
+++ MAPP Projekt Grupp10/Assets/Scripts/ThrowRockScript.cs
@@ -12,7 +12,7 @@ public class ThrowRockScript : MonoBehaviour
         rb.velocity = Vector2.right * bulletSpeed;
     }
 
-    void OnCollisionEnter2D(Collision2D collision){
+    private void OnTriggerEnter2D(Collider2D collision){
         Destroy(gameObject);
     }
 
